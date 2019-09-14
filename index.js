c0f5fe661c237bbd51839cebf94b42563a120c12
kenpachi-at-312088930561 <m.lewis1337@gmail.com>
2019-09-14 19:15:12 -0400

    corrected errors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { exec } = require('child_process');
+const path = require('path');
 
 class ServerlessPlugin {
   constructor(serverless, options) {
@@ -18,23 +19,42 @@ class ServerlessPlugin {
       console.error("GO is required to run this plugin and GOPATH was not found!")
     }
 
+    const sericeRuntime = this.serverless.service.provider.runtime;
+    const servicePath = this.serverless.config.servicePath;
+
     const functionNames = this.serverless.service.getAllFunctions()
     const functions = functionNames
       .map((func) => this.serverless.service.getFunction(func))
-      .filter((func) => func.runtime.includes("go"))
+      .filter((func) => {
+        if (sericeRuntime.includes("go") && typeof func.runtime === "undefined") {
+          return true
+        } else if (typeof func.runtime !== "undefined") {
+          return func.runtime.includes("go")
+        } else {
+          return false
+        }
+      })
 
     functions.forEach(async (func) => {
 
         try {
-            const output = func.handler;
+
+            let build = null
+            if (typeof func.build !== "undefined") {
+              build = path.join(servicePath, func.build)
+            }
+
+            const output = path.join(servicePath, func.handler);
             
-            exec(`go build -o ${output}`, (error) => {
-              if (error) {
-                console.error(`exec error: ${error}`);
-                return;
-              }
-            });
-            
+            if (build) {
+
+              exec(`cd ${build} ; GOOS=linux go build -ldflags="-s -w" -o ${output}`, (error) => {
+                if (error) {
+                  console.error(`exec error: ${error}`);
+                  return;
+                }
+              });
+            }
         } catch(err) {
             console.error(err);
         }
